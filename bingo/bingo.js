9f60d2316658e43bac03b2db0220b45e410196dc
alsersan <alsersan20@gmail.com>
2021-08-23 19:27:29 +0200

    Finish bingo

--- bingo/bingo.js
+++ bingo/bingo.js
@@ -1,19 +1,16 @@
 function bingo() {
-  // const username = prompt(`What's your name?`);
-  let cardNumbers;
-  let satisfied = false;
-  do {
-    cardNumbers = generateCardNumbers();
-    showCard(cardNumbers);
-    satisfied = confirm(
-      `Do you want to play with this card? Press 'OK' to continue or 'Cancel' to generate another one`
-    );
-  } while (!satisfied);
+  console.clear();
+  const username = askName();
+  printScoreSystem(username);
+
+  const cardNumbers = generateCardNumbers();
   const calledNumbers = [];
-  const lineBingo = {
+  const winners = {
     oneLineBingo: false,
     twoLineBingo: false,
+    fullHouseBingo: false,
   };
+  let points = 100;
   let newTurn = true;
 
   do {
@@ -24,19 +21,24 @@ function bingo() {
       cardNumbers[numIndex] += ballNumber > 9 ? '       X' : '         X';
       showCard(cardNumbers);
     }
-    newTurn = checkWinner(cardNumbers, lineBingo);
+    points += checkWinner(cardNumbers, winners);
+    !winners.fullHouseBingo
+      ? (newTurn = confirm('Do you want to pick another number?'))
+      : (newTurn = false);
   } while (newTurn);
-  const playAgain = confirm('Do you want to play again?');
-  if (playAgain) {
-    console.clear();
-    bingo();
+  if (winners.fullHouseBingo) {
+    printFinalScore(username, points);
+    const playAgain = confirm('Do you want to play again?');
+    playAgain && bingo();
   }
 }
 bingo();
 
-function checkWinner(numbers, lineBingo) {
+function checkWinner(numbers, winners) {
   const card = [numbers.slice(0, 5), numbers.slice(5, 10), numbers.slice(10, 15)];
   let fullLines = 0;
+  // Each turn reduces 1 point from the score
+  let points = -1;
   card.forEach((line) => {
     let counter = 0;
     line.forEach((number) => {
@@ -44,19 +46,22 @@ function checkWinner(numbers, lineBingo) {
     });
     if (counter === 5) fullLines++;
   });
-  if (fullLines === 1 && !lineBingo.oneLineBingo) {
+  if (fullLines === 1 && !winners.oneLineBingo) {
     alert('You have a One-Line Bingo!');
-    lineBingo.oneLineBingo = true;
+    winners.oneLineBingo = true;
+    points += 50;
   }
-  if (fullLines === 2 && !lineBingo.twoLineBingo) {
+  if (fullLines === 2 && !winners.twoLineBingo) {
     alert('You have a Two-Line Bingo!');
-    lineBingo.twoLineBingo = true;
+    winners.twoLineBingo = true;
+    points += 100;
   }
   if (fullLines === 3) {
     alert('You have a Full House Bingo!');
-    return null;
+    winners.fullHouseBingo = true;
+    points += 200;
   }
-  return confirm('Do you want to pick another number?');
+  return points;
 }
 
 function generateCardNumbers() {
@@ -64,6 +69,11 @@ function generateCardNumbers() {
   while (nums.size < 15) {
     nums.add(Math.floor(Math.random() * (90 - 1) + 1));
   }
+  showCard([...nums]);
+  const satisfied = confirm(
+    `Do you want to play with this card? Press 'OK' to continue or 'Cancel' to generate another one`
+  );
+  if (!satisfied) return generateCardNumbers();
   return [...nums];
 }
 
@@ -79,4 +89,49 @@ function generateBallNumber(calledNumbers) {
 
 function showCard(numbers) {
   console.table([numbers.slice(0, 5), numbers.slice(5, 10), numbers.slice(10, 15)]);
+}
+
+function askName() {
+  const username = prompt(`What's your name?`)?.trim();
+  return username ? username : 'guest';
+}
+
+function printScoreSystem(name) {
+  alert(`
+    Hello ${name}. Your initial score is 100.
+    Your final score will be calculated based on these rules:
+
+    Each turn: -1 point
+    One-Line Bingo: 50 points
+    Two Line Bingo: 100 points
+    Full House Bingo: 200 points
+  `);
+}
+
+function printFinalScore(username, points) {
+  const dummyResults = [
+    {
+      username: 'Potatoman',
+      points: 68,
+    },
+    {
+      username: 'Da_best',
+      points: 230,
+    },
+    {
+      username: 'Eternal loser',
+      points: 10,
+    },
+    {
+      username,
+      points,
+    },
+  ];
+  let leaderboard = '';
+  dummyResults
+    .sort((a, b) => b.points - a.points)
+    .forEach((result) => {
+      leaderboard += `${result.username}: ${result.points} points \n`;
+    });
+  alert(`Leaderboard\n\n${leaderboard}`);
 }
